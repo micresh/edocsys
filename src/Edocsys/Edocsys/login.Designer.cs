353b28019e95c582a47d3aaf9010232d417573aa
micresh <micresh@localhost>
2012-09-26 13:51:14 +0400

    add possibility to open useradm && databaseconnection

--- src/Edocsys/Edocsys/login.Designer.cs
+++ src/Edocsys/Edocsys/login.Designer.cs
@@ -28,20 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.issl1DataSet = new Edocsys.issl1DataSet();
+            this.issl1DataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.usersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.usersTableAdapter = new Edocsys.issl1DataSetTableAdapters.usersTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.issl1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.issl1DataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.usersBindingSource)).BeginInit();
             this.SuspendLayout();
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(28, 88);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(194, 20);
+            this.maskedTextBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(140, 134);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataSource = this.usersBindingSource;
+            this.comboBox1.DisplayMember = "login";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(28, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(194, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // issl1DataSet
+            // 
+            this.issl1DataSet.DataSetName = "issl1DataSet";
+            this.issl1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // issl1DataSetBindingSource
+            // 
+            this.issl1DataSetBindingSource.DataSource = this.issl1DataSet;
+            this.issl1DataSetBindingSource.Position = 0;
+            // 
+            // usersBindingSource
+            // 
+            this.usersBindingSource.DataMember = "users";
+            this.usersBindingSource.DataSource = this.issl1DataSetBindingSource;
+            // 
+            // usersTableAdapter
+            // 
+            this.usersTableAdapter.ClearBeforeFill = true;
             // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(496, 266);
+            this.ClientSize = new System.Drawing.Size(347, 203);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextBox1);
             this.Name = "LoginForm";
             this.Text = "loginform";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.LoginForm_FormClosed);
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.issl1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.issl1DataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.usersBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.BindingSource issl1DataSetBindingSource;
+        private issl1DataSet issl1DataSet;
+        private System.Windows.Forms.BindingSource usersBindingSource;
+        private issl1DataSetTableAdapters.usersTableAdapter usersTableAdapter;
     }
 }
--- src/Edocsys/Edocsys/login.cs
+++ src/Edocsys/Edocsys/login.cs
@@ -22,5 +22,12 @@ namespace Edocsys
         {
             formcount = false;
         }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'issl1DataSet.users' table. You can move, or remove it, as needed.
+            this.usersTableAdapter.Fill(this.issl1DataSet.users);
+
+        }
     }
 }
