fe5db298355f1f17606c8a465c9c29de06268c6c
micresh <micresh@localhost>
2012-10-01 13:46:11 +0400

    final type of sql script

--- db/createbase.sql
+++ db/createbase.sql
@@ -1,48 +1,51 @@
+DROP DATABASE IF EXISTS `Edocbase`;
+
 CREATE DATABASE IF NOT EXISTS `Edocbase`;
 
 GRANT ALL PRIVILEGES ON *.* TO root@localhost IDENTIFIED BY 'po12jein45bf';
 
 CREATE  TABLE IF NOT EXISTS `Edocbase`.`Agents` (
   `idAgents` INT NOT NULL AUTO_INCREMENT ,
-  `Ag_fullname` VARCHAR(45) NULL ,
-  `Ag_shortname` VARCHAR(45) NULL ,
-  `Ag_addr` VARCHAR(45) NULL ,
-  `Ag_tel` VARCHAR(45) NULL ,
-  `Ag_fax` VARCHAR(45) NULL ,
-  `Ag_mail` VARCHAR(45) NULL ,
-  `Ag_INN` VARCHAR(45) NULL ,
-  `Ag_OGRN` VARCHAR(45) NULL ,
-  `Ag_KPP` VARCHAR(45) NULL ,
-  `Ag_OKPO` VARCHAR(45) NULL ,
-  `Ag_Rcou` VARCHAR(45) NULL ,
-  `Ag_Kcou` VARCHAR(45) NULL ,
-  `Ag_BIK` VARCHAR(45) NULL ,
-  `Ag_bank` VARCHAR(45) NULL ,
+  `Ag_fullname` VARCHAR(127) NULL ,
+  `Ag_shortname` VARCHAR(63) NULL ,
+  `Ag_addr` VARCHAR(255) NULL ,
+  `Ag_tel` VARCHAR(15) NULL ,
+  `Ag_fax` VARCHAR(15) NULL ,
+  `Ag_mail` VARCHAR(31) NULL ,
+  `Ag_INN` VARCHAR(15) NULL ,
+  `Ag_OGRN` VARCHAR(15) NULL ,
+  `Ag_KPP` VARCHAR(15) NULL ,
+  `Ag_OKPO` VARCHAR(15) NULL ,
+  `Ag_Rcou` VARCHAR(15) NULL ,
+  `Ag_Kcou` VARCHAR(15) NULL ,
+  `Ag_BIK` VARCHAR(15) NULL ,
+  `Ag_bank` VARCHAR(127) NULL ,
   `Ag_doc` VARCHAR(45) NULL ,
   `Ag_pers_status` VARCHAR(45) NULL ,
-  `Ag_pers_fullname` VARCHAR(45) NULL ,
-  `Ag_pers_shortname` VARCHAR(45) NULL ,
+  `Ag_pers_firstname` VARCHAR(30) NULL ,
+  `Ag_pers_lastname` VARCHAR(30) NULL ,
+  `Ag_pers_middlename` VARCHAR(30) NULL ,
   PRIMARY KEY (`idAgents`) )
 ENGINE = InnoDB;
 CREATE  TABLE IF NOT EXISTS `Edocbase`.`Experts` (
   `idExperts` INT NOT NULL AUTO_INCREMENT ,
-  `Expert_Lastname` VARCHAR(45) NULL ,
-  `Expert_Firstname` VARCHAR(45) NULL ,
-  `Expert_Middlename` VARCHAR(45) NULL ,
+  `Expert_Lastname` VARCHAR(30) NULL ,
+  `Expert_Firstname` VARCHAR(30) NULL ,
+  `Expert_Middlename` VARCHAR(30) NULL ,
   PRIMARY KEY (`idExperts`) )
 ENGINE = InnoDB;
 CREATE  TABLE IF NOT EXISTS `Edocbase`.`Products` (
   `idProducts` INT NOT NULL AUTO_INCREMENT ,
   `Product_name` VARCHAR(45) NULL ,
-  `Product_OKP` VARCHAR(45) NULL ,
-  `Product_TNVED` VARCHAR(45) NULL ,
+  `Product_OKP` VARCHAR(31) NULL ,
+  `Product_TNVED` VARCHAR(31) NULL ,
   PRIMARY KEY (`idProducts`) )
 ENGINE = InnoDB;
 CREATE  TABLE IF NOT EXISTS `Edocbase`.`ProdGost` (
   `id` INT NOT NULL ,
   `idProducts` INT NULL ,
-  `GOST_numb` VARCHAR(45) NULL ,
-  `Prod_type` VARCHAR(45) NULL ,
+  `GOST_numb` VARCHAR(15) NULL ,
+  `Prod_type` VARCHAR(31) NULL ,
   PRIMARY KEY (`id`) ,
   INDEX `fk_ProdGost_1` (`idProducts` ASC) ,
   CONSTRAINT `fk_ProdGost_1`
@@ -56,8 +59,8 @@ CREATE  TABLE IF NOT EXISTS `Edocbase`.`Contracts` (
   `idProducts` INT NULL ,
   `emission_type` VARCHAR(45) NULL ,
   `Client_docs` VARCHAR(45) NULL ,
-  `Schem_type` INT NULL ,
-  `Add_data` VARCHAR(45) NULL ,
+  `Schem_type` VARCHAR(45) NULL ,
+  `Add_data` TEXT NULL ,
   `Contract_type` TINYINT NULL ,
   `Contract_value_1` MEDIUMINT NULL ,
   `Contract_value_full` MEDIUMINT NULL ,
