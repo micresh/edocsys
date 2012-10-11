126a72791276b45ee58fc306dd8998fa1ee6c177
micresh <micresh@localhost>
2012-10-11 18:35:47 +0400

    add defaultuser to usercreate script

--- db/createusers.sql
+++ db/createusers.sql
@@ -1,3 +1,4 @@
 GRANT ALL PRIVILEGES ON Edocbase.* TO admin@localhost IDENTIFIED BY 'wepo23nri_)(*';
 GRANT ALL PRIVILEGES ON Edocbase.* TO expert@localhost IDENTIFIED BY 'wepo23nri_)(*';
 GRANT ALL PRIVILEGES ON Edocbase.* TO direktor@localhost IDENTIFIED BY 'wepo23nri_)(*';
+GRANT SELECT ON Edocbase.users TO defuser@localhost IDENTIFIED BY 'ttrr';
