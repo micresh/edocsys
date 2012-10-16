e30ce4ce42f6c4e7682a486786ce529e8f0bc834
volikag <volikag@localhost>
2012-10-17 00:34:04 +0400

    DB:     - change users data with proper password's hashes

--- db/datainsert.sql
+++ db/datainsert.sql
@@ -1,5 +1,5 @@
 -- isnert users into users table
-INSERT INTO `edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('admin','e9e587ea8c027bb751ecffc2fe2756f4',1,NULL);
-INSERT INTO `edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('expert','e9e587ea8c027bb751ecffc2fe2756f4',1,NULL);
-INSERT INTO `edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('direktor','e9e587ea8c027bb751ecffc2fe2756f4',1,NULL);
-INSERT INTO `edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('defuser','e9e587ea8c027bb751ecffc2fe2756f4',1,NULL);
+INSERT INTO `edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('admin','e9e587ea8c027bb751ecffc2fe2756f4',1,'admin');
+INSERT INTO `edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('expert','e9e587ea8c027bb751ecffc2fe2756f4',1,'expert');
+INSERT INTO `edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('direktor','e9e587ea8c027bb751ecffc2fe2756f4',1,'direktor');
+INSERT INTO `edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('defuser','71426716f97ec95d806e5593afd02691',1,'defuser');
