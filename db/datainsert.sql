0ae52259cecd17a603cda10c9952d0dc2ac91109
volikag <volikag@localhost>
2012-10-17 04:06:55 +0400

    DB:     - update sqls for users insert

--- db/datainsert.sql
+++ db/datainsert.sql
@@ -1,5 +1,5 @@
 -- isnert users into users table
-INSERT INTO `edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('admin','e9e587ea8c027bb751ecffc2fe2756f4',1,'admin');
-INSERT INTO `edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('expert','e9e587ea8c027bb751ecffc2fe2756f4',1,'expert');
-INSERT INTO `edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('direktor','e9e587ea8c027bb751ecffc2fe2756f4',1,'direktor');
-INSERT INTO `edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('defuser','71426716f97ec95d806e5593afd02691',1,'defuser');
+INSERT INTO `edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('admin', md5('123'),1,'admin');
+INSERT INTO `edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('expert', md5('123'),1,'expert');
+INSERT INTO `edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('direktor', md5('123'),1,'direktor');
+INSERT INTO `edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('defuser', md5('123'),1,'defuser');
