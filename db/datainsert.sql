1c34c579ddce2b022623c1d11074c932a7d6ec87
volikag <volikag@localhost>
2012-10-17 21:12:33 +0400

    Project:     - add Thumbs.db to ignore list
    DB:
        - change cratebase.sql (add 2 tables)
        - change datainsert.sql (add data for reference data)
        - add createbase.sh for databse deployment

--- db/datainsert.sql
+++ db/datainsert.sql
@@ -1,5 +1,22 @@
--- isnert users into users table
-INSERT INTO `edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('admin', md5('123'),1,'admin');
-INSERT INTO `edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('expert', md5('123'),1,'expert');
-INSERT INTO `edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('direktor', md5('123'),1,'direktor');
-INSERT INTO `edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('defuser', md5('123'),1,'defuser');
+﻿-- isnert users into users table
+INSERT INTO `Edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('admin', md5('123'),1,'admin');
+INSERT INTO `Edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('expert', md5('123'),1,'expert');
+INSERT INTO `Edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('direktor', md5('123'),1,'direktor');
+INSERT INTO `Edocbase`.`users` (`login`, `password`, `type`, `log_database`) VALUES ('defuser', md5('123'),1,'defuser');
+-- isnert contracttypes into ContractTypes table
+INSERT INTO `Edocbase`.`ContractTypes` (`id`, `name`) VALUES (0,'заявка добавлена');
+INSERT INTO `Edocbase`.`ContractTypes` (`id`, `name`) VALUES (1,'к заявке прикреплен эксперт');
+INSERT INTO `Edocbase`.`ContractTypes` (`id`, `name`) VALUES (2,'начата работа по заявке');
+INSERT INTO `Edocbase`.`ContractTypes` (`id`, `name`) VALUES (3,'оформлен акт выполненных работ');
+INSERT INTO `Edocbase`.`ContractTypes` (`id`, `name`) VALUES (4,'акт утвержден руководителем');
+INSERT INTO `Edocbase`.`ContractTypes` (`id`, `name`) VALUES (20,'невыполнение по вине заказчика');
+INSERT INTO `Edocbase`.`ContractTypes` (`id`, `name`) VALUES (21,'невыполнение по вине эксперта');
+-- isnert types into PaymentTypes table
+INSERT INTO `Edocbase`.`PaymentTypes` (`id`, `name`) VALUES (10,'договор оформлен');
+INSERT INTO `Edocbase`.`PaymentTypes` (`id`, `name`) VALUES (11,'договор подписан');
+INSERT INTO `Edocbase`.`PaymentTypes` (`id`, `name`) VALUES (12,'перечислен аванс');
+INSERT INTO `Edocbase`.`PaymentTypes` (`id`, `name`) VALUES (13,'перечислена полная сумма');
+INSERT INTO `Edocbase`.`PaymentTypes` (`id`, `name`) VALUES (14,'догвор исполнен');
+
+
+
