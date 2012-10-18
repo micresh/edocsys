1b167733df4f38185bdbfb5aeefa04a79733d08e
VolikAG <VolikAG@vm1>
2012-10-18 18:39:53 +0400

    DB:     - Change useradd script (now can connect from any host)

--- db/createusers.sql
+++ db/createusers.sql
@@ -1,3 +1,3 @@
-GRANT ALL PRIVILEGES ON Edocbase.* TO admin@localhost IDENTIFIED BY 'wepo23nri_)(*';
-GRANT ALL PRIVILEGES ON Edocbase.* TO expert@localhost IDENTIFIED BY 'wepo23nri_)(*';
-GRANT ALL PRIVILEGES ON Edocbase.* TO direktor@localhost IDENTIFIED BY 'wepo23nri_)(*';
+GRANT ALL PRIVILEGES ON Edocbase.* TO admin IDENTIFIED BY 'wepo23nri_)(*';
+GRANT ALL PRIVILEGES ON Edocbase.* TO expert IDENTIFIED BY 'wepo23nri_)(*';
+GRANT ALL PRIVILEGES ON Edocbase.* TO direktor IDENTIFIED BY 'wepo23nri_)(*';
