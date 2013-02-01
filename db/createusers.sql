f02e81b0ca31e13e53a5c310f8abaa1fb6698254
volikag <volikag@localhost>
2013-02-01 17:56:28 +0400

    DB:     * change create users script

--- db/createusers.sql
+++ db/createusers.sql
@@ -1,3 +1,1 @@
-GRANT ALL PRIVILEGES ON Edocbase.* TO admin IDENTIFIED BY 'wepo23nri_)(*';
-GRANT ALL PRIVILEGES ON Edocbase.* TO expert IDENTIFIED BY 'wepo23nri_)(*';
-GRANT ALL PRIVILEGES ON Edocbase.* TO direktor IDENTIFIED BY 'wepo23nri_)(*';
+GRANT ALL PRIVILEGES ON edocbase.* TO admin IDENTIFIED BY 'wepo23nri_)(*';
