00fb6cc4ed95e2fcdd3671acc33560f79def7a35
volikag <volikag@localhost>
2013-03-17 01:44:40 +0400

    Project:     * Add trace helper

---
+++ src/Edocsys/Edocsys/TraceHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Diagnostics;
+
+namespace Edocsys
+{
+    static class TraceHelper
+    {
+        public static void LogError(string msg, Exception ex)
+        {
+            Trace.Fail((DateTime.Now).ToString());
+            Trace.Fail(msg);
+            Trace.Fail(ex.Source);
+            Trace.Fail(ex.Message);
+        }
+    }
+}
