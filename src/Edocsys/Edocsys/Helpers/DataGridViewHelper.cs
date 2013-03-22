65562f0eaeb6fc7d954332345f7273ee029e23fc
volikag <volikag@localhost>
2013-03-23 00:16:53 +0400

    DataGridViewHelper:     * Change highlighting colors     * Add Column order/width saving prototype

--- src/Edocsys/Edocsys/Helpers/DataGridViewHelper.cs
+++ src/Edocsys/Edocsys/Helpers/DataGridViewHelper.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Collections.Generic;
+
+using System.Configuration;
 
 using System.Reflection;
 
@@ -27,6 +30,87 @@ namespace Edocsys
         }
 
 
+        [Serializable]
+        public sealed class ColumnOrderItem
+        {
+            public int DisplayIndex { get; set; }
+            public int Width { get; set; }
+            public bool Visible { get; set; }
+            public int ColumnIndex { get; set; }
+        }
+
+        private static void SetColumnOrder(DataGridView grid)
+        {
+            if (!gfDataGridViewSetting.Default.ColumnOrder.ContainsKey(grid.Name))
+                return;
+
+            List<ColumnOrderItem> columnOrder =
+                gfDataGridViewSetting.Default.ColumnOrder[grid.Name];
+
+            if (columnOrder != null)
+            {
+                //var sorted = columnOrder.OrderBy(i => i.DisplayIndex);
+                
+                //foreach (var item in sorted)
+                foreach (var item in columnOrder)
+                {
+                    grid.Columns[item.ColumnIndex].DisplayIndex =
+                                        item.DisplayIndex;
+                    grid.Columns[item.ColumnIndex].Visible = item.Visible;
+                    grid.Columns[item.ColumnIndex].Width = item.Width;
+                }
+            }
+        }
+
+        internal sealed class gfDataGridViewSetting : ApplicationSettingsBase
+        {
+            private static gfDataGridViewSetting _defaultInstace =
+                (gfDataGridViewSetting)ApplicationSettingsBase
+                .Synchronized(new gfDataGridViewSetting());
+            //---------------------------------------------------------------------
+            public static gfDataGridViewSetting Default
+            {
+                get { return _defaultInstace; }
+            }
+            //---------------------------------------------------------------------
+            // Because there can be more than one DGV in the user-application
+            // a dictionary is used to save the settings for this DGV.
+            // As key the name of the control is used.
+            [UserScopedSetting]
+            [SettingsSerializeAs(SettingsSerializeAs.Binary)]
+            [DefaultSettingValue("")]
+            public Dictionary<string, List<ColumnOrderItem>> ColumnOrder
+            {
+                get
+                {
+                    return this["ColumnOrder"] as Dictionary<string,
+                             List<ColumnOrderItem>>;
+                }
+                set { this["ColumnOrder"] = value; }
+            }
+        }
+        private static void SaveColumnOrder(DataGridView grid)
+        {
+            if (grid.AllowUserToOrderColumns)
+            {
+                List<ColumnOrderItem> columnOrder = new List<ColumnOrderItem>();
+                DataGridViewColumnCollection columns = grid.Columns;
+                for (int i = 0; i < columns.Count; i++)
+                {
+                    columnOrder.Add(new ColumnOrderItem
+                    {
+                        ColumnIndex = i,
+                        DisplayIndex = columns[i].DisplayIndex,
+                        Visible = columns[i].Visible,
+                        Width = columns[i].Width
+                    });
+                }
+
+                gfDataGridViewSetting.Default.ColumnOrder[grid.Name] = columnOrder;
+                gfDataGridViewSetting.Default.Save();
+            }
+        }
+
         public static void ChangeGridRowColor(DataGridViewRow row, int days_left)
         {
             Color bk, sbk;
@@ -38,17 +122,20 @@ namespace Edocsys
 
             if (days_left < (int)Constants.DeadlineAlerts.Fortnight)
             {
-                bk = Color.FromArgb(0xFF, 0xCB, 0xDB);
+                //yellow
+                bk = Color.FromArgb(0xFF, 0xCC, 0x00);
                 sbk = Color.FromArgb(255, 0, 0);
             }
             if (days_left < (int)Constants.DeadlineAlerts.Week)
             {
-                bk = Color.FromArgb(0xFA, 0xDA, 0xDD);
+                //orenge
+                bk = Color.FromArgb(0xFF, 0x66, 0x33);
                 sbk = Color.FromArgb(255, 0, 0);
             }
             if (days_left < (int)Constants.DeadlineAlerts.Overdue)
             {
-                bk = Color.FromArgb(0xFD, 0xD7, 0xE4);
+                //pink
+                bk = Color.FromArgb(0xFF, 0x66, 0xCC);
                 sbk = Color.FromArgb(255, 0, 0);
             }
 
