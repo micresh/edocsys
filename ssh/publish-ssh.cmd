78c9faf4ffc35dc462d68bebd1c4cc465ae6bdd2
volikag <volikag@localhost>
2015-08-12 00:17:21 +0300

    * Fix deployment script backup

--- ssh/publish-ssh.cmd
+++ ssh/publish-ssh.cmd
@@ -3,6 +3,6 @@ SET PWD=po12jein45bf
 SET HOST=kisp.no-ip.org
 SET PORT=22
 SET PUBLISHDIR=../src/Edocsys/Edocsys/publish
-plink.exe -pw %PWD% -batch -v -P %PORT% %USER%@%HOST% `mkdir /opt/edocsys/updates/backup-\`date +%%y-%%m-%%d-%%H-%%M\``
-plink.exe -pw %PWD% -batch -v -P %PORT% %USER%@%HOST% `mv /opt/edocsys/updates/lastest/* /opt/edocsys/updates/backup-\`date +%%y-%%m-%%d-%%H-%%M\``
+
+plink.exe -pw %PWD% -batch -v -P %PORT% %USER%@%HOST% `export BUDATE=\`date +%%y-%%m-%%d-%%H-%%M\` ; mkdir /opt/edocsys/updates/backup-$BUDATE ; mv /opt/edocsys/updates/lastest/* /opt/edocsys/updates/backup-$BUDATE`
 pscp.exe  -pw %PWD% -batch -v -sftp -P %PORT% -r %PUBLISHDIR%/* %USER%@%HOST%:/opt/edocsys/updates/lastest/
